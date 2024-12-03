845eac762ad7d1d80937bd963286fac1fbbbeb8f
dhyeyyyyyyyy <153886201+dhyeyyyyyyyy@users.noreply.github.com>
2024-12-03 19:46:15 +0530

    Update bot.py

--- File-Store-Bot-Token-Verification-codeflix/File-Store-Bot-Token-Verification-codeflix/bot.py
+++ File-Store-Bot-Token-Verification-codeflix/File-Store-Bot-Token-Verification-codeflix/bot.py
@@ -14,8 +14,8 @@ class Bot(Client):
     def __init__(self):
         super().__init__(
             name="Bot",
-            api_hash=API_HASH,
-            api_id=APP_ID,
+            api_hash=e34d594cbce9a3d968ffb41a147e1a80,
+            api_id=21806330,
             plugins={
                 "root": "plugins"
             },
@@ -40,7 +40,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/codeflix_bots for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/dhyeygotyou for support")
                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
@@ -50,12 +50,12 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/codeflix_bots for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/dhyeygotyou for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/codeflix_bots")
-        self.LOGGER(__name__).info(f""" BOT DEPLOYED BY @CODEFLIX_BOTS""")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/dhyeygotyou")
+        self.LOGGER(__name__).info(f""" BOT DEPLOYED BY @dhyeygotyou""")
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
